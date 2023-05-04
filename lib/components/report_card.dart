5360eaffab2f589a87a45be24934044b236ec394
tomurango <tomuraeishi@tomuraeishinoMacBook-Air.local>
2023-05-04 11:24:43 +0900

    カード押下で情報を編集可能にした

--- lib/components/report_card.dart
+++ lib/components/report_card.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 
 class ReportCard extends StatefulWidget {
@@ -57,6 +58,93 @@ class _ReportCardState extends State<ReportCard> {
         borderRadius: BorderRadius.circular(10),
       ),
       */
+    );
+  }
+}*/
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ReportCard extends StatefulWidget {
+  final String title;
+  final String memo;
+  final DateTime diaryDate;
+
+  ReportCard({required this.title, required this.memo, required this.diaryDate});
+
+  @override
+  _ReportCardState createState() => _ReportCardState();
+}
+
+class _ReportCardState extends State<ReportCard> {
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _memoController = TextEditingController();
+  bool _isEditing = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController.text = widget.title;
+    _memoController.text = widget.memo;
+  }
+
+  void _toggleEditing() {
+    setState(() {
+      _isEditing = !_isEditing;
+    });
+  }
+
+  Future<void> _saveData() async {
+    CollectionReference reports =
+        FirebaseFirestore.instance.collection('reports');
+    await reports.doc(widget.diaryDate.toString()).set({
+      'title': _titleController.text,
+      'memo': _memoController.text,
+      'date': widget.diaryDate,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        if (_isEditing) {
+          _saveData();
+        }
+        _toggleEditing();
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _isEditing
+                  ? TextField(
+                      controller: _titleController,
+                      decoration: InputDecoration(
+                        labelText: 'Title',
+                        border: OutlineInputBorder(),
+                      ),
+                    )
+                  : Text(
+                      widget.title,
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+              SizedBox(height: 8),
+              _isEditing
+                  ? TextField(
+                      controller: _memoController,
+                      decoration: InputDecoration(
+                        labelText: 'Memo',
+                        border: OutlineInputBorder(),
+                      ),
+                    )
+                  : Text(widget.memo),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
